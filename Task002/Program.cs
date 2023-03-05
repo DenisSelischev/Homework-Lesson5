96c03ec404f046d56501908618ccfdcbf258f3ca
Denis <Deni-SelischeV23@yandex.ru>
2023-03-05 19:30:17 +0300

    Silved 2 task

---
+++ Task002/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArray(numbers);
+Console.WriteLine("В массиве: ");
+PrintArray(numbers);
+int sum = 0;
+
+
+for (int i = 1; i < numbers.Length; i=i+2)
+    sum = sum + numbers[i];
+
+    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+void FillArray(int[] numbers)
+{
+    for(int j = 0; j < numbers.Length; j++)
+        {
+            numbers[j] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    for(int k = 0; k < numbers.Length; k++)
+        {
+            Console.Write(numbers[k] + " ");
+        }
+    Console.WriteLine();
+}
